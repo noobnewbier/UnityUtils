6078d6556273b3cad8be773076bf71eda8122dfb
noobnewbier <peter789b@gmail.com>
2019-03-12 18:26:56 +0000

    update third person cam

--- ThirdPersonCamera.cs
+++ ThirdPersonCamera.cs
@@ -9,13 +9,8 @@ using System.Collections;
 [RequireComponent(typeof(Camera))]
 public class ThirdPersonCamera : MonoBehaviour
 {
-    [SerializeField] Transform _overviewPosition;
-
-    public float smooth = 3f;       // カメラモーションのスムーズ化用変数
-    Transform _standardPos;          // the usual position for the camera, specified by a transform in the game
-    Transform _frontPos;         // Front Camera locater
-    Transform _jumpPos;          // Jump Camera locater
-    bool _isOverview = false;
+    [SerializeField] Transform _cameraPosition;          // the usual position for the camera, specified by a transform in the game
+    [SerializeField] float _smooth = 3f;       // カメラモーションのスムーズ化用変数
     Camera _camera;
 
 
@@ -26,43 +21,22 @@ public class ThirdPersonCamera : MonoBehaviour
             Destroy(this);
             return;
         }
-        // 各参照の初期化
-        _standardPos = GameObject.Find("CamPos").transform;
         _camera = GetComponent<Camera>();
 
-        transform.position = _standardPos.position;
-        transform.forward = _standardPos.forward;
+        transform.position = _cameraPosition.position;
+        transform.forward = _cameraPosition.forward;
     }
 
 
     void FixedUpdate()  // このカメラ切り替えはFixedUpdate()内でないと正常に動かない
     {
-        if (Input.GetMouseButtonDown(1))
-        {
-            ToggleCameraState();
-        }
-        SetCameraPosition(_isOverview ? _overviewPosition : _standardPos);
+        SetCameraPosition(_cameraPosition);
     }
-
-    void ToggleCameraState()
-    {
-        _isOverview = !_isOverview;
-        int shootAreaLayer = 1 << LayerMask.NameToLayer("ShootArea");
-
-        if (_isOverview)
-        {
-            _camera.cullingMask |= shootAreaLayer;
-        }
-        else
-        {
-            _camera.cullingMask ^= shootAreaLayer;
-        }
-    }
-
+    
     void SetCameraPosition(Transform targetTransform)
     {
-        transform.position = Vector3.Lerp(transform.position, targetTransform.position, Time.fixedDeltaTime * smooth);
-        transform.forward = Vector3.Lerp(transform.forward, targetTransform.forward, Time.fixedDeltaTime * smooth);
+        transform.position = Vector3.Lerp(transform.position, targetTransform.position, Time.fixedDeltaTime * _smooth);
+        transform.forward = Vector3.Lerp(transform.forward, targetTransform.forward, Time.fixedDeltaTime * _smooth);
     }
   
 }
