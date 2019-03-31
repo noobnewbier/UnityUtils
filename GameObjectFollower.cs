a8d500c1af6aa18ddbd02fb66ab4cca260a604e4
noobnewbier <peter789b@gmail.com>
2019-03-31 12:34:43 +0100

    -added gameobject follower

---
+++ GameObjectFollower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectFollower : MonoBehaviour
+{
+
+    [SerializeField] Transform _transformToFollow;
+    [SerializeField] float _smooth = 15f;       // カメラモーションのスムーズ化用変数
+    Vector3 _offset;
+
+
+    void Start()
+    {
+        _offset = transform.position - _transformToFollow.position;
+    }
+
+
+    void FixedUpdate()  // このカメラ切り替えはFixedUpdate()内でないと正常に動かない
+    {
+        SetPosition((_transformToFollow.rotation * _offset) + _transformToFollow.position);
+    }
+
+    void SetPosition(Vector3 targetPosition)
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * _smooth);
+    }
+}
