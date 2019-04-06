23517fc434533713d25b269f1bca66815ecee1b3
noobnewbier <peter789b@gmail.com>
2019-04-06 12:35:08 +0100

    collsion bridge, more interface to be added

---
+++ CollisionBridge.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CollisionBridge : MonoBehaviour
+{
+    [SerializeField] MonoBehaviour _delegate;
+
+    //todo: include other events
+    private void OnCollisionEnter(Collision collision)
+    {
+        var collisionEnterDelegate = _delegate as ICollisionEnterDelegate;
+        if (_delegate != null)
+            collisionEnterDelegate.OnCollisionEnter(collision);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var triggerExitDelegate = _delegate as ITriggerExitDelegate;
+
+        if (_delegate != null)
+            triggerExitDelegate.OnTriggerExit(other);
+    }
+}
+
+public interface ICollisionEnterDelegate
+{
+    void OnCollisionEnter(Collision collision);
+}
+
+public interface ITriggerExitDelegate
+{
+    void OnTriggerExit(Collider other);
+}
+
+
+
