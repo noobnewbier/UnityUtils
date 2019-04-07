464721fba003e3e66bce23a1c0c76b1d448f279c
noobnewbier <peter789b@gmail.com>
2019-04-07 18:56:33 +0100

    AutoDestroyParticle System: destroy gameobject after particle system is no longer alive

---
+++ AutoDestroyParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class AutoDestroyParticle : MonoBehaviour
+{
+
+    ParticleSystem _particleSystem;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (!_particleSystem.IsAlive()) Destroy(gameObject);
+    }
+}
