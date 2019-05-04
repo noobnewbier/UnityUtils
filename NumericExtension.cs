ab8b870d055daebad87d3abbd848003cbd4f6a0d
noobnewbier7 <peter789b@gmail.com>
2019-05-04 02:58:12 +0100

    Added float util for float comparison

---
+++ NumericExtension.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace NumericExtension
+{
+    public static class FloatUtil
+    {
+        public static bool NearlyEqual(float a, float b, float epsilon)
+        {
+            float absA = Mathf.Abs(a);
+            float absB = Mathf.Abs(b);
+            float diff = Mathf.Abs(a - b);
+
+            if (a == b)
+            { // shortcut, handles infinities
+                return true;
+            }
+            else if (a == 0 || b == 0 || diff < float.MinValue)
+            {
+                // a or b is zero or both are extremely close to it
+                // relative error is less meaningful here
+                return diff < (epsilon * float.MinValue);
+            }
+            else
+            { // use relative error
+                return diff / (absA + absB) < epsilon;
+            }
+        }
+    }
+}
