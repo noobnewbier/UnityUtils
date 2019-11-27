b7254bcbffd4f1133453591102d4d32e5204f047
noobnewbier7 <peter789b@gmail.com>
2019-11-27 04:40:09 +0000

    Added EnumerableExtension for replacing stuffs

---
+++ EnumerableExtension.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace UnityUtils
+{
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<T> ReplaceAll<T>(this IEnumerable<T> source, T oldValue, T newValue, IEqualityComparer<T> comparer = null)
+        {       
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            comparer = comparer ?? EqualityComparer<T>.Default;
+
+            foreach (var item in source)
+            {
+                yield return
+                    comparer.Equals(item, oldValue)
+                        ? newValue
+                        : item;
+            }
+        }
+    }
+}
