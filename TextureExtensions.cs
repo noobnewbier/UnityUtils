15bf3e02a52611cc9f155e585b01847ae3c91b1b
noobnewbier7 <peter789b@gmail.com>
2019-12-11 03:58:14 +0000

    optimzing texture extension so no extra texture is created

--- TextureExtensions.cs
+++ TextureExtensions.cs
@@ -5,16 +5,19 @@ namespace UnityUtils
 {
     public static class TextureExtensions
     {
-        #region scrap anc clean up fromhttps://forum.unity.com/threads/rotate-a-texture-with-an-arbitrary-angle.23904/
+        #region scrap and clean up fromhttps://forum.unity.com/threads/rotate-a-texture-with-an-arbitrary-angle.23904/
 
         [SuppressMessage("ReSharper", "InconsistentNaming")]
-        public static Texture2D RotateTexture(this Texture2D tex, float angle)
+        public static void RotateTexture(this Texture2D tex, float angle, Color defaultColor = default)
         {
-            var rotImage = new Texture2D(tex.width, tex.height);
-            int x;
 
             var w = tex.width;
             var h = tex.height;
+            var pixelsCount = w * h;
+            var originalPixels = tex.GetPixels();
+            var newPixels = new Color[pixelsCount];
+            
+            
             var x0 = rot_x(angle, -w / 2.0f, -h / 2.0f) + w / 2.0f;
             var y0 = rot_y(angle, -w / 2.0f, -h / 2.0f) + h / 2.0f;
 
@@ -23,47 +26,42 @@ namespace UnityUtils
             var dy_x = rot_x(angle, 0.0f, 1.0f);
             var dy_y = rot_y(angle, 0.0f, 1.0f);
 
-
             var x1 = x0;
             var y1 = y0;
 
-            for (x = 0; x < tex.width; x++)
+            for (var x = 0; x < w; x++)
             {
                 var x2 = x1;
                 var y2 = y1;
-                int y;
-                for (y = 0; y < tex.height; y++)
+                for (var y = 0; y < h; y++)
                 {          
                     x2 += dx_x; //rot_x(angle, x1, y1);
                     y2 += dx_y; //rot_y(angle, x1, y1);
-                    rotImage.SetPixel((int) Mathf.Floor(x), (int) Mathf.Floor(y), GetPixel(tex, x2, y2));
+
+                    var x2Int = (int) x2;
+                    var y2Int = (int) y2;
+
+                    Color color;
+                    
+                    if (x2Int >= w || x2Int < 0 ||
+                        y2Int >= h || y2Int < 0)
+                    {
+                        color = defaultColor;
+                    }
+                    else
+                    {
+                        color = originalPixels[y2Int * w + x2Int];
+                    }
+                    
+                    newPixels[y * w + x] = color;
                 }
 
                 x1 += dy_x;
                 y1 += dy_y;
             }
-
-            rotImage.Apply();
-            return rotImage;
-        }
-
-        private static Color GetPixel(Texture2D tex, float x, float y)
-        {
-            Color pix;
-            var x1 = (int) Mathf.Floor(x);
-            var y1 = (int) Mathf.Floor(y);
-
-            if (x1 > tex.width || x1 < 0 ||
-                y1 > tex.height || y1 < 0)
-            {
-                pix = Color.clear;
-            }
-            else
-            {
-                pix = tex.GetPixel(x1, y1);
-            }
-
-            return pix;
+            
+            tex.SetPixels(newPixels);
+            tex.Apply();
         }
 
         private static float rot_x(float angle, float x, float y)
