d13ab2f66c5a2ec4d9c8535bba4a10b9d20cace5
noobnewbier7 <peter789b@gmail.com>
2019-12-28 09:55:24 +0000

    Added functionality for auto saving scens on play

---
+++ Editor/AutoSaveOnRunMenuItem.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+//https://forum.unity.com/threads/we-need-auto-save-feature.483853/
+namespace UnityUtils.Editor
+{
+    [InitializeOnLoad]
+    public class AutoSaveOnRunMenuItem
+    {
+        private const string MenuName = "Tools/Autosave On Run";
+        private static bool _isToggled;
+
+        static AutoSaveOnRunMenuItem()
+        {
+            EditorApplication.delayCall += () =>
+            {
+                _isToggled = EditorPrefs.GetBool(MenuName, false);
+                Menu.SetChecked(MenuName, _isToggled);
+                SetMode();
+            };
+        }
+
+        [MenuItem(MenuName)]
+        private static void ToggleMode()
+        {
+            _isToggled = !_isToggled;
+            Menu.SetChecked(MenuName, _isToggled);
+            EditorPrefs.SetBool(MenuName, _isToggled);
+            SetMode();
+        }
+
+        private static void SetMode()
+        {
+            if (_isToggled)
+            {
+                EditorApplication.playModeStateChanged += AutoSaveOnRun;
+            }
+            else
+            {
+                EditorApplication.playModeStateChanged -= AutoSaveOnRun;
+            }
+        }
+
+        private static void AutoSaveOnRun(PlayModeStateChange state)
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                Debug.Log("Auto-Saving before entering Play mode");
+
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+}
