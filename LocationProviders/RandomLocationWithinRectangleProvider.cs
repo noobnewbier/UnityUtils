4805a385d1bf8e57eecd45ca324526583bacc5f3
noobnewbier7 <peter789b@gmail.com>
2020-04-04 00:20:56 +0100

    Added RandomLocationWithinRectangleProvider

---
+++ LocationProviders/RandomLocationWithinRectangleProvider.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace UnityUtils.LocationProviders
+{
+    public class RandomLocationWithinRectangleProvider : LocationProvider
+    {
+        [SerializeField] private float halfLength;
+        [SerializeField] private float halfWidth;
+
+        public override Vector3 ProvideLocation()
+        {
+            var localPosition = transform.localPosition;
+            var x = localPosition.x + Random.Range(-halfWidth, halfWidth);
+            var z = localPosition.z + Random.Range(-halfLength, halfLength);
+            var targetPosition = new Vector3(x, localPosition.y, z);
+            targetPosition = transform.TransformPoint(targetPosition);
+            return targetPosition;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            var color = Gizmos.color;
+            Gizmos.color = Color.red;
+
+            var localPosition = transform.localPosition;
+            var leftX = localPosition.x - halfWidth;
+            var rightX = localPosition.x + halfWidth;
+            var botZ = localPosition.z - halfLength;
+            var topZ = localPosition.z + halfLength;
+
+            var topLeft = transform.TransformPoint(new Vector3(leftX, localPosition.y, topZ));
+            var botLeft = transform.TransformPoint(new Vector3(leftX, localPosition.y, botZ));
+            var topRight = transform.TransformPoint(new Vector3(rightX, localPosition.y, topZ));
+            var botRight = transform.TransformPoint(new Vector3(rightX, localPosition.y, botZ));
+
+            Gizmos.DrawLine(topLeft, botLeft);
+            Gizmos.DrawLine(topLeft, topRight);
+            Gizmos.DrawLine(botLeft, botRight);
+            Gizmos.DrawLine(botRight, topRight);
+
+            Gizmos.color = color;
+        }
+    }
+}
