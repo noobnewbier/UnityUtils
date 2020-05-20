250920e33fffad2894c91a27704716cac37b9257
noobnewbier7 <peter789b@gmail.com>
2020-05-20 02:17:30 +0100

    Added Editor Utility for replacing Go

---
+++ Editor/ReplaceGameObjects.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtils.Editor
+{
+// CopyComponents - by Michael L. Croswell for Colorado Game Coders, LLC
+// March 2010
+
+//Modified by Kristian Helle Jespersen
+//June 2011
+
+    public class ReplaceGameObjects : ScriptableWizard
+    {
+        public GameObject newType;
+        public GameObject[] oldObjects;
+
+        [MenuItem("Custom/Replace GameObjects")]
+        private static void CreateWizard()
+        {
+            DisplayWizard("Replace GameObjects", typeof(ReplaceGameObjects), "Replace");
+        }
+
+        private void OnWizardCreate()
+        {
+            foreach (var go in oldObjects)
+            {
+                var newObject = (GameObject) PrefabUtility.InstantiatePrefab(newType);
+                newObject.transform.position = go.transform.position;
+                newObject.transform.rotation = go.transform.rotation;
+                newObject.transform.parent = go.transform.parent;
+
+                DestroyImmediate(go);
+            }
+        }
+    }
+}
