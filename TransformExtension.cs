1ffdf64d87bf1e5122637ae806f9b43d1c8b8174
noobnewbier7 <peter789b@gmail.com>
2020-08-04 18:19:55 +0800

    Second phase of renaming extension 2 commit ago /shrug

---
+++ TransformExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace UnityUtils
+{
+    public static class TransformExtension
+    {
+        public static Transform FindChildWithTag(this Transform transform, string tag)
+        {
+            return transform.Cast<Transform>().FirstOrDefault(child => child.CompareTag(tag));
+        }
+    }
+}
