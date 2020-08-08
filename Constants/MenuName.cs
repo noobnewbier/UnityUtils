dd88aab4ad382689c5d7b5821e1a497da49c61ab
noobnewbier7 <peter789b@gmail.com>
2020-08-08 15:15:33 +0800

    Added "ScriptableRepository" in MenuName

--- Constants/MenuName.cs
+++ Constants/MenuName.cs
@@ -3,6 +3,7 @@
     public static class MenuName
     {    
         public const string ScriptableService = "ScriptableService/";
+        public const string ScriptableRepository = "ScriptableRepository/";
         public const string Providers = "Providers/";
         public const string Data = "Data/";
     }
