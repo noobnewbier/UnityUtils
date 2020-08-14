fdfc225103bf9c766460b90ab0ccac4b1c836be8
noobnewbier7 <peter789b@gmail.com>
2020-08-14 18:01:18 +0800

    Added concept of RuntimeReference

---
+++ ScriptableReference/RuntimeReference.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityUtils.ScriptableReference
+{
+    public class RuntimeReference<T> : ScriptableObject
+    {
+        [SerializeField] private T initialReference;
+        [SerializeField] private T currentReference;
+        [SerializeField] private bool canBeNullWhenAccessed;
+
+
+        public T CurrentReference
+        {
+            get
+            {
+                if (currentReference == null && !canBeNullWhenAccessed)
+                {
+                    throw new InvalidOperationException($"{name} current reference cannot be null when accessed, does it requires instantiation?");
+                }
+
+                return currentReference;
+            }
+            set
+            {
+                if (value == null & !canBeNullWhenAccessed)
+                {
+                    throw new InvalidOperationException($"{name} current reference cannot be set to null");
+                }
+                
+                currentReference = value;  
+            } 
+        }
+
+        private void OnEnable()
+        {
+            currentReference = initialReference;
+        }
+    }
+}
