f59b525965b84347231359a1d80cee88c1f6cafa
noobnewbier7 <peter789b@gmail.com>
2020-08-22 21:12:42 +0800

    Added linq extension MaxBy and MinBy

--- EnumerableExtension.cs
+++ EnumerableExtension.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UnityUtils
 {
@@ -10,15 +11,28 @@ namespace UnityUtils
                                                    T newValue,
                                                    IEqualityComparer<T> comparer = null)
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
 
             comparer = comparer ?? EqualityComparer<T>.Default;
 
             foreach (var item in source)
                 yield return
-                    comparer.Equals(item, oldValue)
-                        ? newValue
-                        : item;
+                    comparer.Equals(item, oldValue) ? newValue : item;
+        }
+
+        //reference : https://stackoverflow.com/questions/3188693/how-can-i-get-linq-to-return-the-object-which-has-the-max-value-for-a-given-prop
+        public static T MaxBy<T, TU>(this IEnumerable<T> data, Func<T, TU> f) where TU : IComparable
+        {
+            return data.Aggregate((i1, i2) => f(i1).CompareTo(f(i2)) > 0 ? i1 : i2);
+        }
+        
+        //reference : https://stackoverflow.com/questions/3188693/how-can-i-get-linq-to-return-the-object-which-has-the-max-value-for-a-given-prop
+        public static T MinBy<T, TU>(this IEnumerable<T> data, Func<T, TU> f) where TU : IComparable
+        {
+            return data.Aggregate((i1, i2) => f(i1).CompareTo(f(i2)) < 0 ? i1 : i2);
         }
     }
 }
