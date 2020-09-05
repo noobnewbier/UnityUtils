6e53eed393fdb1be8f49f7590a29b5f5e3c2dd6f
noobnewbier7 <peter789b@gmail.com>
2020-09-05 16:25:16 +0800

    Slight clean up on our basic object pool... this can be improved in the future

--- ObjectPool.cs
+++ ObjectPool.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UnityUtils
 {
     public class ObjectPool : MonoBehaviour
     {
-        private const int PoolSize = 50;
         private readonly Stack<PooledMonoBehaviour> _pool = new Stack<PooledMonoBehaviour>();
 
-        private PooledMonoBehaviour _pooledMono;
-
-        public PooledMonoBehaviour PooledGameObject
-        {
-            set => _pooledMono = value;
-        }
-
+        [SerializeField] private PooledMonoBehaviour pooledMono;
+        [Range(0, 10000)] [SerializeField] private int poolSize;
 
         public T GetInstance<T>() where T : PooledMonoBehaviour
         {
@@ -26,22 +22,33 @@ namespace UnityUtils
         {
             GameObject toReturn;
             if (!_pool.Any())
-                toReturn = Instantiate(_pooledMono.gameObject);
+            {
+                toReturn = Instantiate(pooledMono.gameObject);
+            }
             else
+            {
                 toReturn = _pool.Pop().gameObject;
+                var toReturnTransform = toReturn.transform;
+
+                toReturnTransform.parent = null;
+                var pooledMonoTransform = pooledMono.transform;
+                toReturnTransform.rotation = pooledMonoTransform.rotation;
+                toReturnTransform.localScale = pooledMonoTransform.localScale;
+                toReturnTransform.position = pooledMonoTransform.position;
+            }
 
             toReturn.SetActive(true);
-            toReturn.transform.SetParent(transform);
 
             return toReturn;
         }
 
         public void AddInstance(PooledMonoBehaviour instance)
         {
-            if (_pool.Count < PoolSize)
+            if (_pool.Count < poolSize)
             {
                 _pool.Push(instance);
                 instance.gameObject.SetActive(false);
+                instance.transform.parent = transform;
             }
             else
             {
@@ -50,22 +57,38 @@ namespace UnityUtils
             }
         }
 
-        public static ObjectPool GetPoolFor(PooledMonoBehaviour toPool)
+        public static ObjectPool GetPoolFor(PooledMonoBehaviour toPool, int poolSize)
         {
-            if (Application.isEditor) //if there are existing pools
+            var existingPool = GameObject.Find(toPool.name + "Pool");
+            if (existingPool)
             {
-                var obj = GameObject.Find(toPool.name + "Pool");
-                if (obj) return obj.GetComponent<ObjectPool>();
+                var currentPool = existingPool.GetComponent<ObjectPool>();
+
+                if (currentPool.pooledMono == toPool)
+                {
+                    return currentPool;
+                }
+                throw new InvalidOperationException(
+                    $"Two pool should not have a same name, something has gone wrong for {toPool.name}'s pooling"
+                );
             }
 
-            var newPool = new GameObject
-            {
-                name = toPool.name + "Pool"
-            };
-            var pool = newPool.AddComponent<ObjectPool>();
-            pool.PooledGameObject = toPool;
+            var pool = new GameObject().AddComponent<ObjectPool>();
+            pool.InitPool(toPool, poolSize);
 
             return pool;
+        }
+
+        private void InitPool(PooledMonoBehaviour pooledMonoBehaviour, int poolSize)
+        {
+            name = GetPoolName(pooledMonoBehaviour);
+            this.poolSize = poolSize;
+            pooledMono = pooledMonoBehaviour;
+        }
+
+        private static string GetPoolName(Object pooledObject)
+        {
+            return pooledObject.name + "Pool";
         }
     }
 }
