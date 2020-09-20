0c64fb1a5bda1d2cea6fa2fec2b293187ecdefab
noobnewbier7 <peter789b@gmail.com>
2020-09-20 16:11:34 +0800

    Introduced IPoolable - this can be further refined in the future

--- ObjectPool.cs
+++ ObjectPool.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityUtils.Pooling;
 using Object = UnityEngine.Object;
 
 namespace UnityUtils
---
+++ Pooling/IPoolable.cs
@@ -0,0 +1,8 @@
+﻿namespace UnityUtils.Pooling
+{
+    public interface IPoolable<out T> where T : class
+    {
+        T GetPooledInstance();
+        void ReturnToPool();
+    }
+}
