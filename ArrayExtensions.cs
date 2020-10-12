c79ea0024f27345cf11d98ed57a80dab6e05d02d
noobnewbier7 <peter789b@gmail.com>
2020-10-12 17:54:21 +0800

    Added ArrayExtensions.cs

---
+++ ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace UnityUtils
+{
+    public static class ArrayExtensions
+    {
+        public static void Fill<T>(this T[] originalArray, T with)
+        {
+            for (var i = 0; i < originalArray.Length; i++)
+            {
+                originalArray[i] = with;
+            }
+        }
+    }
+}
