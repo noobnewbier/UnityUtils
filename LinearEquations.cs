5812907f9ae9f881b2bea865034cbe5f04b59b40
noobnewbier <peter789b@gmail.com>
2021-08-16 04:17:43 +0900

    Renamed line equation to linearequation

---
+++ LinearEquations.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace UnityUtils
+{
+    public static class LinearEquations
+    {
+        //reference: https://math.stackexchange.com/questions/404440/what-is-the-equation-for-a-3d-line symmetric form
+        public static (float x, float z) GetXzGivenY(float targetY, Vector3 origin, Vector3 endPoint)
+        {
+            var direction = endPoint - origin;
+
+            var yEquationAnswer = (targetY - origin.y) / direction.y;
+
+            var x = direction.x * yEquationAnswer + origin.x;
+            var z = direction.z * yEquationAnswer + origin.z;
+
+            return (x, z);
+        }
+
+        public class LinearEquation2D
+        {
+            private readonly float _constantValue;
+            private readonly float _xCoefficient;
+            private readonly float _yCoefficient;
+
+            public LinearEquation2D(float xCoefficient, float yCoefficient, float constantValue)
+            {
+                _xCoefficient = xCoefficient;
+                _yCoefficient = yCoefficient;
+                _constantValue = constantValue;
+            }
+
+            public float Solve(Vector2 input) => _xCoefficient * input.x + _yCoefficient * input.y - _constantValue;
+        }
+    }
+}
