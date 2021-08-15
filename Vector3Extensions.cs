f47b5d8209618f79a342a1d1e9db51491118aac6
noobnewbier <peter789b@gmail.com>
2021-08-16 04:20:41 +0900

    added Vector3Extensions.cs

---
+++ Vector3Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UnityUtils
+{
+    public static class Vector3Extensions
+    {
+        public static Vector2 XZ(this Vector3 vector3) => new Vector2(vector3.x, vector3.z);
+    }
+}
