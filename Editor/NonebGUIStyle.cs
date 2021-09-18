c16b339debb1be92bb56f05faa954afe7e41a866
noobnewbier <peter789b@gmail.com>
2021-09-18 14:32:24 +0900

    Added editor for Unit
    - this mainly serves as a proof of concept, tbh not sure how well the partial class approach will work

---
+++ Editor/NonebGUIStyle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtils.Editor
+{
+    public static class NonebGUIStyle
+    {
+        public static readonly GUIStyle Normal = EditorStyles.label;
+
+        public static readonly GUIStyle Error = new GUIStyle(EditorStyles.label)
+        {
+            normal = new GUIStyleState
+            {
+                textColor = Color.red
+            }
+        };
+
+        public static readonly GUIStyle Hint = new GUIStyle(EditorStyles.label)
+        {
+            normal = new GUIStyleState
+            {
+                textColor = Color.grey
+            }
+        };
+    }
+}
