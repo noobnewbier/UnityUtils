97e5ba7b7109cd373775bddd1333a69c0f3b48ca
noobnewbier <peter789b@gmail.com>
2021-10-03 22:21:39 +0900

    slight clean up on utils

--- LinearEquations.cs
+++ LinearEquations.cs
@@ -5,10 +5,8 @@ namespace UnityUtils
     public static class LinearEquations
     {
         //reference: https://math.stackexchange.com/questions/404440/what-is-the-equation-for-a-3d-line symmetric form
-        public static (float x, float z) GetXzGivenY(float targetY, Vector3 origin, Vector3 endPoint)
+        public static (float x, float z) GetXzGivenY(float targetY, Vector3 origin, Vector3 direction)
         {
-            var direction = endPoint - origin;
-
             var yEquationAnswer = (targetY - origin.y) / direction.y;
 
             var x = direction.x * yEquationAnswer + origin.x;
