c4e87bb9736f3fa84a9cd5633d57ff142e8f7310
noobnewbier <peter789b@gmail.com>
2021-11-03 04:29:45 +0900

    Adding/Changing existing styles

--- Editor/NonebGUIStyle.cs
+++ Editor/NonebGUIStyle.cs
@@ -1,10 +1,13 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace UnityUtils.Editor
 {
     public static class NonebGUIStyle
     {
+        private static readonly Lazy<Texture2D> SceneHelpBoxBackgroundTexture = new Lazy<Texture2D>(CreateBackgroundTexture);
+
         public static readonly GUIStyle Normal = EditorStyles.label;
 
         public static readonly GUIStyle Error = new GUIStyle(EditorStyles.label)
@@ -19,8 +22,39 @@ namespace UnityUtils.Editor
         {
             normal = new GUIStyleState
             {
-                textColor = Color.grey
+                textColor = new Color(0.62f, 0.62f, 0.62f)
             }
         };
+
+        public static readonly GUIStyle Title = new GUIStyle(EditorStyles.whiteLabel)
+        {
+            alignment = TextAnchor.MiddleCenter
+        };
+
+        public static readonly GUIStyle SceneHelpBox = new GUIStyle(EditorStyles.helpBox)
+        {
+            normal = new GUIStyleState
+            {
+                background = SceneHelpBoxBackgroundTexture.Value
+            }
+        };
+
+        private static Texture2D CreateBackgroundTexture()
+        {
+            var texture = new Texture2D(1, 1);
+            texture.SetPixel(
+                0,
+                0,
+                new Color(
+                    0.19f,
+                    0.19f,
+                    0.19f,
+                    0.65f
+                )
+            );
+            texture.Apply();
+
+            return texture;
+        }
     }
 }
