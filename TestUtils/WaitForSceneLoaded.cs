5fbe5c3c30e0a0269045e60a2af08e12372e87fc
noobnewbier <peter789b@gmail.com>
2021-12-30 00:58:05 +0900

    Added TestUtils.WaitForSceneLoaded

---
+++ TestUtils/WaitForSceneLoaded.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+namespace UnityUtils.TestUtils
+{
+    public class WaitForSceneLoaded : CustomYieldInstruction
+    {
+        private readonly string _sceneName;
+        private readonly float _startTime;
+        private readonly float _timeout;
+
+        public bool TimedOut { get; private set; }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                var scene = SceneManager.GetSceneByName(_sceneName);
+                var sceneLoaded = scene.IsValid() && scene.isLoaded;
+
+                if (Time.realtimeSinceStartup - _startTime >= _timeout) TimedOut = true;
+
+                Assert.IsFalse(TimedOut, $"TimedOut when loading {_sceneName}");
+                return !sceneLoaded && !TimedOut;
+            }
+        }
+
+        public WaitForSceneLoaded(string newSceneName, float newTimeout = 10)
+        {
+            _sceneName = newSceneName;
+            _timeout = newTimeout;
+            _startTime = Time.realtimeSinceStartup;
+        }
+    }
+}
