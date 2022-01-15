13c1ea2588992a81078f576d02f3c8ceabae4006
noobnewbier <peter789b@gmail.com>
2022-01-15 23:58:23 +0900

    Added NonebEditorGUI.ShowStringPopup

---
+++ Editor/NonebEditorGUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtils.Editor
+{
+    public static class NonebEditorGUI
+    {
+        public static void ShowStringPopup(Rect rect,
+                                           SerializedProperty property,
+                                           string label,
+                                           string[] options)
+        {
+            var currentIndex = Array.IndexOf(options, property.stringValue);
+            var newIndex = EditorGUI.Popup(
+                rect,
+                label,
+                currentIndex,
+                options
+            );
+            if (currentIndex != newIndex) property.stringValue = options[newIndex];
+        }
+    }
+}
