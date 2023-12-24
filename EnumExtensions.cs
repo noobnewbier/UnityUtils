32a1cb94f80d96d0635e51abbcd6792c5c36b0c8
noobnewbier <peter789b@gmail.com>
2023-12-24 18:40:50 +0000

    feat(extension): adding EnumExtensions

---
+++ EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityUtils
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<T> GetFlags<T>(this T input) where T : Enum
+        {
+            foreach (Enum value in Enum.GetValues(typeof(T)))
+                if (input.HasFlag(value))
+                    yield return (T)value;
+        }
+    }
+}
