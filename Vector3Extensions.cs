006286323c468be33637564af7dc7a14121258da
noobnewbier <peter789b@gmail.com>
2024-11-06 19:38:23 +0000

    feat: adding more vec3 utils

--- Vector3Extensions.cs
+++ Vector3Extensions.cs
@@ -5,5 +5,11 @@ namespace UnityUtils
     public static class Vector3Extensions
     {
         public static Vector2 XZ(this Vector3 vector3) => new Vector2(vector3.x, vector3.z);
+
+        public static bool NearlyEqual(this Vector3 a, Vector3 b, float epsilon = 0.005f)
+        {
+            var dist = Vector3.Distance(a, b);
+            return dist.NearlyEqual(0f, epsilon);
+        }
     }
 }
