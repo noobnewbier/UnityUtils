f26cd9d44b682e5c32c2ac0bd0afede435d2aa11
noobnewbier <peter789b@gmail.com>
2024-11-28 01:23:28 +0000

    feat: adding a myriad of editor utils

--- Editor/NonebEditorUtils.cs
+++ Editor/NonebEditorUtils.cs
@@ -1,16 +1,130 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using Object = UnityEngine.Object;
 
 namespace UnityUtils.Editor
 {
     public static class NonebEditorUtils
     {
+        /// <summary>
+        /// I really can't be asked to do reflection just to get that, if future me really want to look, go check
+        /// UnityEditor.IMGUI.Controls.TreeViewGUI.cs
+        /// </summary>
+        public static float FoldoutIconWidth
+        {
+            get
+            {
+                const float foldoutIconWidth = 11f;
+                const float padding = 3f;
+
+                return foldoutIconWidth + padding;
+            }
+        }
+
+        public static bool CanShowInOneLine(this Type type) =>
+            type.IsPrimitive ||
+            type.IsEnum ||
+            type == typeof(string) ||
+            type == typeof(Color) ||
+            type == typeof(AnimationCurve) ||
+            type == typeof(Gradient) ||
+            type.IsSameOrSubclass(typeof(Object)) ||
+            type.IsSameOrSubclass(typeof(Addressables));
+
         public static string GetPropertyBindingPath(string propertyName) => $"<{propertyName}>k__BackingField";
+
+        public static Rect ShiftRect(Rect a, Rect b) => ShiftRect(a, b.xMin - a.xMin + b.width);
+
+        public static Rect ShiftRect(Rect rect, float shiftInX)
+        {
+            const float padding = 2;
+            var toReturn = rect;
+            toReturn.width -= shiftInX + padding;
+            toReturn.x += shiftInX + padding;
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// EditorGUI.PropertyField but also drawing the foldout to group them up.
+        /// </summary>
+        public static Rect DrawDefaultPropertyWithFoldout(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var foldoutRect = position;
+            foldoutRect.height = EditorGUIUtility.singleLineHeight;
+            foldoutRect.width = EditorGUIUtility.labelWidth;
+            property.isExpanded = EditorGUI.Foldout(foldoutRect, property.isExpanded, label);
+            if (!property.isExpanded) return position;
+
+            var fieldRect = foldoutRect;
+            fieldRect.y = foldoutRect.y + EditorGUIUtility.standardVerticalSpacing + EditorGUIUtility.singleLineHeight;
+
+            //Keep the unindented one to return.
+            position = fieldRect;
+
+            using (new EditorGUI.IndentLevelScope())
+            {
+                fieldRect = EditorGUI.IndentedRect(fieldRect);
+
+                EditorGUI.PropertyField(fieldRect, property, true);
+            }
+
+            return position;
+        }
+
+        public static SerializedProperty? FindParentProperty(this SerializedProperty property)
+        {
+            var propertyPaths = property.propertyPath.Split('.');
+            if (propertyPaths.Length <= 1) return default;
+
+            var parentSerializedProperty = property.serializedObject.FindProperty(propertyPaths.First());
+            for (var index = 1; index < propertyPaths.Length - 1; index++)
+                if (propertyPaths[index] == "Array" && propertyPaths.Length > index + 1 && Regex.IsMatch(propertyPaths[index + 1], "^data\\[\\d+\\]$"))
+                {
+                    var match = Regex.Match(propertyPaths[index + 1], "^data\\[(\\d+)\\]$");
+                    var arrayIndex = int.Parse(match.Groups[1].Value);
+                    parentSerializedProperty = parentSerializedProperty.GetArrayElementAtIndex(arrayIndex);
+                    index++;
+                }
+                else
+                {
+                    parentSerializedProperty = parentSerializedProperty.FindPropertyRelative(propertyPaths[index]);
+                }
+
+            return parentSerializedProperty;
+        }
+
+        public static T? GetEnumValueFromProperty<T>(this SerializedProperty property) where T : Enum
+        {
+            var index = property.enumValueIndex;
+            var name = property.enumNames.ElementAtOrDefault(index);
+            if (string.IsNullOrEmpty(name)) return default;
+
+            if (!Enum.TryParse(typeof(T), name, true, out var result)) return default;
+
+            return (T)result;
+        }
+
+        public static SerializedProperty? NFindPropertyRelative(this SerializedProperty self, string propertyName)
+        {
+            //default to find field
+            var toReturn = self.FindPropertyRelative(propertyName);
+            if (toReturn != null) return toReturn;
+
+            //fallback to properties backing field
+            var backingFieldPath = GetPropertyBindingPath(propertyName);
+            toReturn = self.FindPropertyRelative(backingFieldPath);
+
+            //that's the best we can do
+            return toReturn;
+        }
 
         public static T? FindPropertyOfTypeAtRoot<T>(this SerializedObject serializedObject) where T : class
         {
@@ -23,6 +137,137 @@ namespace UnityUtils.Editor
             } while (prop.Next(false));
 
             return default;
+        }
+
+        /// <summary>
+        /// Gets the object the property represents.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static object? GetTargetObjectOfProperty(SerializedProperty prop)
+        {
+#if UNITY_2021_2_OR_NEWER
+            if (prop.propertyType == SerializedPropertyType.ManagedReference) return prop.managedReferenceValue;
+#elif UNITY_2022_1_OR_NEWER
+            return prop.boxedValue;
+#endif
+
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object? obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements)
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("[", StringComparison.Ordinal));
+                    var index = Convert.ToInt32(element.Substring(element.IndexOf("[", StringComparison.Ordinal)).Replace("[", "").Replace("]", ""));
+
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Gets the object that the property is a member of
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static object? GetTargetObjectWithProperty(SerializedProperty prop)
+        {
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object? obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements.Take(elements.Length - 1))
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("[", StringComparison.Ordinal));
+                    var index = Convert.ToInt32(element.Substring(element.IndexOf("[", StringComparison.Ordinal)).Replace("[", "").Replace("]", ""));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+
+            return obj;
+        }
+
+        public static object?[] GetTargetObjectsWithProperty(SerializedProperty prop)
+        {
+            if (prop.serializedObject.targetObjects.Length == 0) return Array.Empty<object>();
+
+            var arr = new object?[prop.serializedObject.targetObjects.Length];
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            var elements = path.Split('.');
+            foreach (var element in elements.Take(elements.Length - 1))
+                if (element.Contains("["))
+                {
+                    var elementName = element[..element.IndexOf("[", StringComparison.Ordinal)];
+                    var index = Convert.ToInt32(element[element.IndexOf("[", StringComparison.Ordinal)..].Replace("[", "").Replace("]", ""));
+                    for (var i = 0; i < arr.Length; i++)
+                    {
+                        var source = arr[i];
+                        if (source == null) continue;
+
+                        arr[i] = GetValue_Imp(source, elementName, index);
+                    }
+                }
+                else
+                {
+                    for (var i = 0; i < arr.Length; i++)
+                    {
+                        var source = arr[i];
+                        if (source == null) continue;
+
+                        arr[i] = GetValue_Imp(source, element);
+                    }
+                }
+
+            return arr;
+        }
+
+        private static object? GetValue_Imp(object? source, string name)
+        {
+            if (source == null) return null;
+
+            var type = source.GetType();
+
+            while (type != null)
+            {
+                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                if (f != null)
+                    return f.GetValue(source);
+
+                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p != null)
+                    return p.GetValue(source, null);
+
+                type = type.BaseType;
+            }
+
+            return null;
+        }
+
+        private static object? GetValue_Imp(object? source, string name, int index)
+        {
+            if (source == null) return null;
+
+            var enumerable = GetValue_Imp(source, name) as IEnumerable;
+            if (enumerable == null) return null;
+            var enm = enumerable.GetEnumerator();
+            //while (index-- >= 0)
+            //    enm.MoveNext();
+            //return enm.Current;
+
+            for (var i = 0; i <= index; i++)
+                if (!enm.MoveNext())
+                    return null;
+
+            return enm.Current;
         }
 
         /// <summary>
@@ -190,7 +435,7 @@ namespace UnityUtils.Editor
                     if (!string.IsNullOrEmpty(pathToType)) pathToArrayElement = $"{pathToType}.{pathToArrayElement}";
                     var arrayElement = serializedObject.FindProperty(pathToArrayElement);
 
-                    var elementValue = arrayElement.boxedValue;
+                    var elementValue = GetTargetObjectOfProperty(arrayElement);
                     var elementType = elementValue?.GetType();
                     if (elementType == null)
                     {
