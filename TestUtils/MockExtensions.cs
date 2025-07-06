8db2c144147b98cce799f28c2b7834ba21a0d42e
noobnewbier <peter789b@gmail.com>
2025-07-06 18:59:31 +0100

    feat(test): adding a way to verify call order in automated test

---
+++ TestUtils/MockExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+
+namespace UnityUtils.TestUtils
+{
+    public static class MockExtensions
+    {
+        // https://stackoverflow.com/a/19711560
+        public static void SetUpCallInOrder<T>(this Mock<T> mock, params Expression<Action<T>>[] expressions) where T : class
+        {
+            // All closures have the same instance of sharedCallCount
+            var sharedCallCount = 0;
+            for (var i = 0; i < expressions.Length; i++)
+            {
+                // make sure it's called
+                var expression = expressions[i];
+                // mock.Verify(expression);
+
+                // make sure it's called in order
+                var expectedCallCount = i;
+                mock.Setup(expression).Callback(
+                    () =>
+                    {
+                        Assert.AreEqual(expectedCallCount, sharedCallCount);
+                        sharedCallCount++;
+                    }
+                ).Verifiable(Times.Once);
+            }
+        }
+    }
+}
